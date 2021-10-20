710b79fe49d9644e616be488d4c2de05c68610ac
michi <kon-eca@koneca.de>
2021-10-20 21:55:30 +0200

    Created the project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Pixelmanager
+#Pixelmanager
+
+With this app one can resize, scale and convert images to BMP and transmit it to Pixelscanner.
+It uses the bluetooth connection of the ESP32 to transfer the data into the Pixelscanner's SD-Card.
---
+++ app/src/main/java/com/koneca/net/pixelmanager/DisplayMessageActivity.java
@@ -0,0 +1,23 @@
+package com.koneca.net.pixelmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ComponentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
